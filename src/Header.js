9a609fcbe97c902074b1ffc2093230a2b63dc9cc
An Glebski <gluhova19962010@mail.ru>
2021-05-22 15:39:02 +0300

    new project

---
+++ src/Header.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function Header() {
+  return(
+    <header>
+      <img src="https://c0.klipartz.com/pngpicture/823/744/gratis-png-gato-gatito-mowe-meme-mr-frijol.png"
+            alt="Problem?" />
+      <p>Meme Generator</p>
+    </header>
+  )
+}
+
+export default Header;
